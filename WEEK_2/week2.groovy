f4de95821da22a4c5d216033a193b1ce559eb125
Chris Trentham <ctre633@mail.midland.edu>
2017-09-12 13:31:18 -0500

    Added First and Last name constants

--- WEEK_2/week2.groovy
+++ WEEK_2/week2.groovy
@@ -1,3 +1,11 @@
 //start of file
 
 println "Hello World"
+
+import static Constants.*
+
+class Constants {
+    static final String FIRST_NAME = "Chris"
+    static final String LAST_NAME = "Trentham"
+    }
+    
