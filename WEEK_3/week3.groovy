fd75a90840c00ca4cf9fb91a893b2c9d9cc5ca19
Chris Trentham <ctre633@mail.midland.edu>
2017-09-19 23:30:29 -0500

    Added Map of student names and grades

--- WEEK_3/week3.groovy
+++ WEEK_3/week3.groovy
@@ -1,1 +1,14 @@
-//Start of Week 3 Project
+/*Start of Week 3 Project
+
+Create an application which calculates the average of multiple lists and prints the average
+along with the correct letter grade and student name to the console.
+
+*/
+
+Map<String, List> studentGrades = [
+    'Chris Trentham' : [99, 87, 90, 100, 80],
+    'Andrew Thompson' : [50, 0, 90, 70, 70],
+    'Jon Doe' : [100, 87, 91, 90, 100],
+    'Jane Doge' : [86, 10, 48, 43, 48],
+    'Jose Silva' : [0, 40, 80, 100, 100]
+]
